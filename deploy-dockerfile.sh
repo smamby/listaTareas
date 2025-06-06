71faa863d9b6c9c674844327a16f4f3b8b31b86d
smamby <s.mamby@gmail.com>
2025-06-06 01:02:52 -0300

    dockerfile arreglado, probando...

--- deploy-dockerfile.sh
+++ deploy-dockerfile.sh
@@ -12,15 +12,7 @@ fi
 
 echo "Ejecutando contenedor de docker para el backend"
 
-docker run -d 
-    -p 3000:3000
-    --name backend-lista-tareas 
-    -v $(pwd):/app 
-    -e DB_HOST=host.docker.internal 
-    -e DB_PORT=3306 
-    -e DB_USER=root 
-    -e DB_PASSWORD=root 
-    -e DB_NAME=lista_tareas_db lista-tareas
+docker run -d -p 3000:3000 --name backend-lista-tareas -v $(pwd):/app -e DB_HOST=host.docker.internal -e DB_PORT=3306 -e DB_USER=root -e DB_PASSWORD=root -e DB_NAME=lista_tareas_db lista-tareas
 
 if [ $? -ne 0 ]; then
     echo "Error al ejecutar el contenedor de Docker"
