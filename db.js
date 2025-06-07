0471a199a0424184b43033cb39faf8dc9db8de07
smamby <s.mamby@gmail.com>
2025-06-07 20:29:08 -0300

    configurando ci-cd

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ async function initializeDbPool() {
 
     const dbConfig = {
         host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
+        user: process.env.DB_USER || 'todouser',
         password: process.env.DB_PASSWORD || 'root',
         database: process.env.DB_NAME || 'lista_tareas_db',
         port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
